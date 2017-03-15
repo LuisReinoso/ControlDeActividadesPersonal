3ac60623d7af3f2bc9f95d2bb462fe1a84770334
luis <lomejordejr@riseup.net>
2017-03-15 10:15:03 -0500

    Agregado modelo Actividad y Estado de animo

--- app/main/controllers/cronometro-ctrl.js
+++ app/main/controllers/cronometro-ctrl.js
@@ -1,37 +1,45 @@
 'use strict';
 angular.module('main')
-.controller('CronometroCtrl', function ($log, $scope, $interval) {
+  .controller('CronometroCtrl', function($log, $scope, $interval) {
 
-  $log.log('Inicio controlador: CronometroCtrl en modulo main:.', this);
+    $log.log('Inicio controlador: CronometroCtrl en modulo main:.', this);
 
-  $scope.horas = 0;
-  $scope.minutos = 0;
-  $scope.segundos = 0;
-  $scope.isContando = false;
+    $scope.horas = 0;
+    $scope.minutos = 0;
+    $scope.segundos = 0;
+    $scope.isContando = false;
 
-  // Funcion que aumenta cada segundo el contador
-  function segundos () {
-    $scope.segundos = $scope.segundos + 1;
-    if ($scope.segundos == 60) {
-      $scope.segundos = 0;
-      $scope.minutos = $scope.minutos + 1;
-      if ($scope.minutos == 60) {
-        $scope.minutos = 0;
-        $scope.horas = $scope.horas + 1;
+    localforage.getItem('actividades').then(function(value) {
+      $scope.actividades = value;
+    });
+
+    localforage.getItem('estadosDeAnimo').then(function(value) {
+      $scope.estadosDeAnimo = value;
+    });
+
+    // Funcion que aumenta cada segundo el contador
+    function segundos() {
+      $scope.segundos = $scope.segundos + 1;
+      if ($scope.segundos == 60) {
+        $scope.segundos = 0;
+        $scope.minutos = $scope.minutos + 1;
+        if ($scope.minutos == 60) {
+          $scope.minutos = 0;
+          $scope.horas = $scope.horas + 1;
+        }
       }
     }
-  }
 
-  // Inicia el cronometro
-  $scope.iniciar = function () {
-    $scope.conteo = $interval(segundos, 1000);
-    $scope.isContando = true;
-  };
+    // Inicia el cronometro
+    $scope.iniciar = function() {
+      $scope.conteo = $interval(segundos, 1000);
+      $scope.isContando = true;
+    };
 
-  // Detiene el cronometro
-  $scope.pausar = function () {
-    $interval.cancel($scope.conteo);
-    $scope.isContando = false;
-  }
+    // Detiene el cronometro
+    $scope.pausar = function() {
+      $interval.cancel($scope.conteo);
+      $scope.isContando = false;
+    }
 
-});
+  });
--- app/main/main.js
+++ app/main/main.js
@@ -1,22 +1,22 @@
 'use strict';
 angular.module('main', [
-  'ionic',
-  'ngCordova',
-  'ui.router',
-  // TODO: load other modules selected during generation
-])
-.config(function ($stateProvider, $urlRouterProvider) {
+    'ionic',
+    'ngCordova',
+    'ui.router',
+    // TODO: load other modules selected during generation
+  ])
+  .config(function($stateProvider, $urlRouterProvider) {
 
-  // ROUTING with ui.router
-  $urlRouterProvider.otherwise('/main/list');
-  $stateProvider
-    // this state is placed in the <ion-nav-view> in the index.html
-    .state('main', {
-      url: '/main',
-      abstract: true,
-      templateUrl: 'main/templates/menu.html',
-      controller: 'MenuCtrl as menu'
-    })
+    // ROUTING with ui.router
+    $urlRouterProvider.otherwise('/main/list');
+    $stateProvider
+      // this state is placed in the <ion-nav-view> in the index.html
+      .state('main', {
+        url: '/main',
+        abstract: true,
+        templateUrl: 'main/templates/menu.html',
+        controller: 'MenuCtrl as menu'
+      })
       .state('main.list', {
         url: '/list',
         views: {
@@ -53,4 +53,214 @@ angular.module('main', [
           }
         }
       });
-});
+  })
+
+  .run(function() {
+
+    // Actividades
+    localforage.getItem('actividades').then(function(value) {
+      // Carga valores por default
+      if (!value) {
+        var actividades = [{
+            'id': 0,
+            'nombre': 'Alimentacion',
+            'imagen': 'img/alimentacion.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 1,
+            'nombre': 'Caminata',
+            'imagen': 'img/caminata.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 2,
+            'nombre': 'Cocinar',
+            'imagen': 'img/cocinar.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 3,
+            'nombre': 'Deporte',
+            'imagen': 'img/deporte.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 4,
+            'nombre': 'Descansar',
+            'imagen': 'img/descansar.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 5,
+            'nombre': 'Escribir',
+            'imagen': 'img/escribir.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 6,
+            'nombre': 'Estudiar',
+            'imagen': 'img/estudiar.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 7,
+            'nombre': 'Higiene',
+            'imagen': 'img/higiene.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 8,
+            'nombre': 'Hogar',
+            'imagen': 'img/hogar.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 9,
+            'nombre': 'Leer',
+            'imagen': 'img/leer.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 10,
+            'nombre': 'Meditar',
+            'imagen': 'img/meditar.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 11,
+            'nombre': 'Ocio',
+            'imagen': 'img/ocio.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 12,
+            'nombre': 'Programar',
+            'imagen': 'img/programar.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 13,
+            'nombre': 'Redes sociales',
+            'imagen': 'img/redesSociales.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 14,
+            'nombre': 'Trabajar',
+            'imagen': 'img/trabajar.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          }
+        ];
+
+        localforage.setItem('actividades', actividades).then(function(value) {
+          console.log(value);
+        }).catch(function(err) {
+          console.log(err);
+        });
+      }
+
+    }).catch(function(err) {
+      console.log(err);
+    });
+
+
+    // Estados de animo
+    localforage.getItem('estadosDeAnimo').then(function(value) {
+      // Carga valores por default
+      if (!value) {
+        var estadosDeAnimo = [{
+            'id': 0,
+            'nombre': 'Calma energia',
+            'imagen': 'img/calmaEnergia.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 1,
+            'nombre': 'Calma cansancio',
+            'imagen': 'img/caminata.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 2,
+            'nombre': 'Tension energía',
+            'imagen': 'img/tensionEnergia.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          },
+          {
+            'id': 3,
+            'nombre': 'Tension cansancio',
+            'imagen': 'img/tensionCansancio.png',
+            'contador': 0,
+            'horas': 0,
+            'minutos': 0,
+            'segundos': 0
+          }
+        ];
+
+        localforage.setItem('estadosDeAnimo', estadosDeAnimo).then(function(value) {
+          console.log(value);
+        }).catch(function(err) {
+          console.log(err);
+        });
+      }
+
+    }).catch(function(err) {
+      console.log(err);
+    });
+
+  });
